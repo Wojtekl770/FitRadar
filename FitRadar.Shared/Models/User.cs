7690016d19868b0f9a17e51857bdc33426d6733b
Wojciech Łapan <Wojtekl770@interia.pl>
2026-07-07 21:19:40 +0200

    user update

--- FitRadar.Shared/Models/User.cs
+++ FitRadar.Shared/Models/User.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace FitRadar.Shared.Models
 {
-    internal class User
+    public class User : IdentityUser<Guid>
     {
     }
 }
